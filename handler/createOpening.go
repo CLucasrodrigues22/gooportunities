e625d332e2bd01364cf095434e10aa6e72a75a93
lucascardoso432 <lucas.cardoso@ezoom.com.br>
2024-07-25 15:32:25 -0300

    feat: add createOPening handler logic

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -1,12 +1,36 @@
 package handler
 
 import (
+	"github.com/CLucasrodrigues22/gooportunities/schemas"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateOpeningHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"msg": "POST opening",
-	})
+	request := CreateOpeningRequest{}
+
+	context.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validate error: %v", err.Error())
+		sendError(context, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&request).Error; err != nil {
+		logger.Errorf("DB Error: %v", err.Error())
+		sendError(context, http.StatusInternalServerError, "Error creating opening.")
+		return
+	}
+
+	sendSuccess(context, "create-opening", opening)
 }
--- schemas/opening.go
+++ schemas/opening.go
@@ -2,14 +2,32 @@ package schemas
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type Opening struct {
 	gorm.Model
-	Role    string
-	Company string
-	Locator string
-	Remote  bool
-	Link    string
-	Salary  int64
+	Role     string
+	Company  string
+	Location string
+	Remote   bool
+	Link     string
+	Salary   int64
+}
+
+type OpeningResponse struct {
+	ID        uint       `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty" sql:"index"`
+	Role      string     `json:"role"`
+	Company   string     `json:"company"`
+	Location  string     `json:"location"`
+	Remote    bool       `json:"remote"`
+	Link      string     `json:"link"`
+	Salary    int64      `json:"salary"`
+}
+
+func (Opening) TableName() string {
+	return "create_opening_requests"
 }
