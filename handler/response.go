071bf0f65a1fb034e1d351646f740a66522eb2b9
lucascardoso432 <lucas.cardoso@ezoom.com.br>
2024-07-25 15:31:47 -0300

    feat: add request and response helpers

---
+++ handler/response.go
@@ -0,0 +1,23 @@
+package handler
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func sendError(context *gin.Context, code int, msg string) {
+	context.Header("Content-Type", "application/json")
+	context.JSON(code, gin.H{
+		"massage": msg,
+		"error":   code,
+	})
+}
+
+func sendSuccess(context *gin.Context, op string, data interface{}) {
+	context.Header("Content-Type", "application/json")
+	context.JSON(http.StatusOK, gin.H{
+		"massage": fmt.Sprintf("operation %s successfull", op),
+		"data":    data,
+	})
+}
