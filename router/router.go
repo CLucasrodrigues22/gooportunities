13d440877875e07ba2a445271ae96da6ebfd586c
lucascardoso432 <lucas.cardoso@ezoom.com.br>
2024-07-24 21:57:34 -0300

    feat: add routes logic

--- router/router.go
+++ router/router.go
@@ -3,15 +3,10 @@ package router
 import "github.com/gin-gonic/gin"
 
 func InitializeRouter() {
-	// Inicializando o Router utilizando as configurações Default do gin
+	// Initialize router
 	router := gin.Default()
-	m := "Olá mundo"
-	// Definindo uma Rota
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": m,
-		})
-	})
-	// Estamos rodando a nossa api
+	// Initialize routes
+	initializeRoutes(router)
+	// Run the server
 	router.Run(":8000") // listen and serve on 0.0.0.0:8080
 }
