43d7bc54ad2cb5e7ffc66b218c094fda06a0c0f2
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2024-07-27 00:49:05 -0300

    feat: Implemented swaggo api documentation

--- handler/response.go
+++ handler/response.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"fmt"
+	"github.com/CLucasrodrigues22/gooportunities/schemas"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -20,4 +21,31 @@ func sendSuccess(context *gin.Context, op string, data interface{}) {
 		"massage": fmt.Sprintf("operation %s successfull", op),
 		"data":    data,
 	})
+}
+
+type ErrorResponse struct {
+	Message   string `json:"message"`
+	ErrorCode string `json:"errorCode"`
+}
+
+type CreateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+
+type DeleteOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+type ShowOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+type ListOpeningsResponse struct {
+	Message string                    `json:"message"`
+	Data    []schemas.OpeningResponse `json:"data"`
+}
+type UpdateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
 }
--- router/routes.go
+++ router/routes.go
@@ -1,13 +1,18 @@
 package router
 
 import (
+	docs "github.com/CLucasrodrigues22/gooportunities/docs"
 	"github.com/CLucasrodrigues22/gooportunities/handler"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	// Initialize handler
 	handler.InitHandler()
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
@@ -16,4 +21,6 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
+	// Initialize Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
